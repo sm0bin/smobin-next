10cc9108879c3d435dbdfd71fdc7cf29614d72c5
GhostMobin <s1810677110@ru.ac.bd>
2024-01-08 22:58:02 +0600

    image showing

--- src/components/ui/Services.tsx
+++ src/components/ui/Services.tsx
@@ -1,4 +1,5 @@
 import getAllServices from "@/utils/getAllServices";
+import Image from "next/image";
 import React from "react";
 
 const Services = async () => {
@@ -9,12 +10,25 @@ const Services = async () => {
     <div>
       <h1>Services</h1>
       <div>
-        {services.map((service: { name: string; description: string }) => (
-          <div key={service.name}>
-            <h2>{service.name}</h2>
-            <p>{service.description}</p>
-          </div>
-        ))}
+        {services.map(
+          (service: {
+            name: string;
+            description: string;
+            thumbnail: string;
+          }) => (
+            <div key={service.name}>
+              <Image
+                src={service.thumbnail}
+                alt={service.name}
+                style={{ width: "100%", height: "auto" }}
+                width={1920}
+                height={1080}
+              />
+              <h2>{service.name}</h2>
+              <p>{service.description}</p>
+            </div>
+          )
+        )}
       </div>
     </div>
   );
